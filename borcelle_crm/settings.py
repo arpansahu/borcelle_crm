57fd6f4c9ae7fa8baa2c4d83691ed680cc558729
arpan sahu <arpanrocks95@gmail.com>
2022-11-18 01:09:24 +0530

    updated settings.py

--- borcelle_crm/settings.py
+++ borcelle_crm/settings.py
@@ -245,9 +245,5 @@ CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
         "LOCATION": config('REDISCLOUD_URL'),
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient"
-        },
-        "KEY_PREFIX": "example"
     }
 }
