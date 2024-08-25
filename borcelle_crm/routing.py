e25ad53a1ef009f5c56a2fbf635a75340cf99427
Arpan Sahu <arpanrocks95@gmail.com>
2024-08-25 12:36:18 +0530

    bumped channels and related versions and routing

--- borcelle_crm/routing.py
+++ borcelle_crm/routing.py
@@ -2,9 +2,14 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from celery_progress.websockets import routing
 from notifications_app.routing import websocket_urlpatterns
+from django.core.asgi import get_asgi_application
+
+django_asgi_app = get_asgi_application()
 
 application = ProtocolTypeRouter({
     # (http->django views is added by default)
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
     'websocket': AuthMiddlewareStack(
         URLRouter(
             routing.urlpatterns +
